77bfeed44c2a146f450ffc1ab9c65763c1dc1719
Sophie <wenxi1988@hotmail.com>
2021-12-31 16:50:55 -0500

    add question

---
+++ 21. Merge Two Sorted Lists/21. Merge Two Sorted Lists/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.Merge_Two_Sorted_Lists
+{
+    class Program
+    {
+        /*You are given the heads of two sorted linked lists list1 and list2.
+         Merge the two lists in a one sorted list. The list should be made by 
+         splicing together the nodes of the first two lists.
+         Return the head of the merged linked list.
+         * 
+         */
+        static void Main(string[] args)
+        {
+        }
+    }
+}
