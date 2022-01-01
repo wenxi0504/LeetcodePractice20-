5abafc9b498232a0b409329d4158991c70bf9898
Sophie <wenxi1988@hotmail.com>
2021-12-31 19:39:25 -0500

    add answer

--- 21. Merge Two Sorted Lists/21. Merge Two Sorted Lists/Program.cs
+++ 21. Merge Two Sorted Lists/21. Merge Two Sorted Lists/Program.cs
@@ -6,6 +6,33 @@ using System.Threading.Tasks;
 
 namespace _21.Merge_Two_Sorted_Lists
 {
+    public class ListNode
+    {
+      public int val;
+      public ListNode next;
+        public int v1;
+        public int v2;
+        public int v3;
+        private ListNode listNode;
+
+        public ListNode(ListNode listNode)
+        {
+            this.listNode = listNode;
+        }
+
+        public ListNode(int val = 0, ListNode next = null)
+      {
+            this.val = val;
+            this.next = next;
+      }
+
+        public ListNode(int v1, int v2, int v3)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+        }
+    }
     class Program
     {
         /*You are given the heads of two sorted linked lists list1 and list2.
@@ -16,6 +43,52 @@ namespace _21.Merge_Two_Sorted_Lists
          */
         static void Main(string[] args)
         {
+            ListNode l1 = new ListNode ( 1, 2, 4 );
+            ListNode l2 = new ListNode(1, 3, 4);
+            ListNode result = new ListNode(MergeTwoLists(l1, l2));
+            Console.WriteLine(string.Join(", ", result));
+            Console.ReadLine();
+
+        }
+
+        public static ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        { //corner case
+            if (list1 == null)
+            {
+                return list2;
+            }
+            if (list2==null)
+            {
+                return list1;
+            }
+
+            // create a ListNode to store result
+            ListNode dummy = new ListNode(-1);
+            ListNode cur = dummy;
+
+            while (list1 !=null && list2!=null)
+            {
+                if (list1.val < list2.val)
+                {
+                    cur.next = list1;
+                    list1 = list1.next;
+
+                }
+                else
+                {
+                    cur.next = list2;
+                    list2 = list2.next;
+                }
+                cur = cur.next;
+            }
+
+            cur.next = list1 != null ? list1 : list2;
+            return dummy.next;
+               
+
+
+
+
         }
     }
 }
